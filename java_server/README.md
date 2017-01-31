b0eda5e33e1c6ceb9361fdb2eb582331e09d13f8
Juraj Machac <jurajmchc@gmail.com>
2017-01-31 16:01:14 +0100

    Initial generated cookbook

---
+++ java_server/README.md
@@ -0,0 +1,4 @@
+# java_server
+
+TODO: Enter the cookbook description here.
+
---
+++ java_server/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: java_server
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
